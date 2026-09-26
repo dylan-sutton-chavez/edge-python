8923b565a66ea373e2c3dbc09711efcbc0a8f99f
dylan-sutton-chavez <c.sutton.dylan@gmail.com>
2026-09-25 22:01:50 -0600

    refactor: the standard library lives in edge-python-std now

--- js/builtins/time/src/index.js
+++
@@ -1,7 +0,0 @@
-import clock from './clock.js';
-import fmt from './fmt.js';
-
-/* Factory consumed by createWorker({ mainThreadModules: { time } }). Composes the clock + fmt slices. */
-export const time = () => Object.assign({}, clock(), fmt());
-
-export default time;
--- std/struct/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-#![cfg_attr(target_arch = "wasm32", no_main)]
-
-extern crate alloc;
-
-wasm_pdk::module_fixed_pool!();
-
-pub mod pack;
